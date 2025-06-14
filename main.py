7751215dbf9caa183bff01c1e79b6e751e017374
joseespanaudea <jose.espana@udea.edu.co>
2025-06-14 16:18:06 -0500

    opcion a

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from clases import Paciente, ImagenMedica, ImagenComun
+import os
 # Diccionarios para almacenar imágenes y pacientes
 dic_pacientes = {}
 dic_imagenes = {}
@@ -14,3 +16,15 @@ def menu():
         print("f) Salir")
 
         opcion = input("Seleccione una opción: ")
+        
+        if opcion == 'a':
+            clave = input("Clave para guardar la imagen médica: ")
+            carpeta = input("Ruta de la carpeta con DICOMs: ")
+            img_medica = ImagenMedica(carpeta)
+            img_medica.cargar_dicoms()
+            img_medica.reconstruir_volumen()
+            if img_medica.get_volumen() is not None:
+                img_medica.mostrar_cortes()
+            dic_imagenes[clave] = img_medica
+            print(f"Imagen médica guardada con clave: {clave}")
+            
